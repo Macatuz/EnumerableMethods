a1b98abb18ead2a7cc906c6a2b7d49ff2291d054
Macatuz <armando29@live.com>
2019-05-08 18:22:16 -0400

    git big approach

--- main.rb
+++ main.rb
@@ -24,24 +24,108 @@ module Enumerable
   end
 
   def my_select
+    if self.respond_to?(:keys)
+      result = Hash.new
+      for i in 0..self.keys.length - 1
+        if yield(self[self.keys[i]]) then result[self.keys[i]] = self[self.keys[i]] end
+      end
+    else
+      result = Array.new
+      for i in 0..self.count - 1
+        if yield(self[i]) then result.push(self[i]) end
+      end
+    end
+    return result
   end
 
   def my_all
+    result = true
+    if self.respond_to?(:keys)
+      result = Hash.new
+      for i in 0..self.keys.length - 1
+        result = false if !yield(i)
+        break if !yield(i)
+      end
+    else
+      result = Array.new
+      for i in 0..self.count - 1
+        result = false if !yield(i)
+        break if !yield(i)
+      end
+    end
+    return result
   end
 
   def my_any
+    result = true
+    if self.respond_to?(:keys)
+      result = Hash.new
+      for i in 0..self.keys.length - 1
+        result = false if !yield(i)
+        break if !yield(i)
+      end
+    else
+      result = Array.new
+      for i in 0..self.count - 1
+        result = false if !yield(i)
+        break if !yield(i)
+      end
+    end
+    return result
   end
 
   def my_none
+    result = true
+    if self.respond_to?(:keys)
+      result = Hash.new
+      for i in 0..self.keys.length - 1
+        result = false if !yield(i)
+        break if !yield(i)
+      end
+    else
+      result = Array.new
+      for i in 0..self.count - 1
+        result = false if !yield(i)
+        break if !yield(i)
+      end
+    end
+    return !result
   end
 
   def my_count
+    if self.respond_to?(:keys)
+      return self.keys.length
+    else
+      return self.count
+    end
+    return result
   end
 
   def my_map
+    if self.respond_to?(:keys)
+      for i in 0..self.keys.length - 1
+        self[self.keys[i]] = yield(self[self.keys[i]])
+      end
+    else
+      for i in 0..self.count - 1
+        self[i] = yield(self[i])
+      end
+    end
   end
 
   def my_inject
+    if self.respond_to?(:keys)
+      result = self[self.keys[0]]
+      for i in 1..self.keys.length - 1
+        result = yield(result, self[self.keys[i]])
+      end
+    else
+      result = self[0]
+      for i in 1..self.count - 1
+        result = yield(result, self[i])
+      end
+    end
+    return result
   end
 end
 
@@ -53,10 +137,7 @@ class Hash
   include Enumerable
 end
 
-x = Array.new
-x[0] = "asd"
-x[1] = "ee"
-x[2] = "qwe"
-x.my_each do |value|
-  p "#{value} #{index}"
-end
+[1, 2, 3, 4, 5].my_each do |value| p value end
+asd = [1, 2, 3, 4, 5].my_select do |value| value > 2 end
+p asd
+p (5..10).inject { |sum, n| sum + n }
