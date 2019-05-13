07c0f620e8c108b122f5ceea09e606ad1300b1ff
Macatuz <armando29@live.com>
2019-05-13 00:29:17 -0400

    expected functions approach - fix bad issues

--- main.rb
+++ main.rb
@@ -1,22 +1,30 @@
 module Enumerable
-  def my_each
-    if self.respond_to?(:keys)
+  def my_each(&block)
+    if self.respond_to?(:keys) && block_given?
       for i in 0..self.keys.length - 1
-        yield(self[self.keys[i]])
+        if block.arity === 2
+          yield(self.keys[i].to_sym, self[self.keys[i]])
+        else
+          yield([self.keys[i].to_sym, self[self.keys[i]]])
+        end
       end
-    else
+    elsif block_given?
       for i in 0..self.count - 1
         yield(self[i], i)
       end
     end
   end
 
-  def my_each_with_index
-    if self.respond_to?(:keys)
+  def my_each_with_index(&block)
+    if self.respond_to?(:keys) && block_given?
       for i in 0..self.keys.length - 1
-        yield(self.keys[i], self[self.keys[i]])
+        if block.arity === 2
+          yield([self.keys[i], self[self.keys[i]]], i)
+        else
+          yield([self.keys[i].to_sym, self[self.keys[i]]])
+        end
       end
-    else
+    elsif block_given?
       for i in 0..self.count - 1
         yield(self[i], i)
       end
@@ -24,12 +32,12 @@ module Enumerable
   end
 
   def my_select
-    if self.respond_to?(:keys)
+    if self.respond_to?(:keys) && block_given?
       result = Hash.new
       for i in 0..self.keys.length - 1
-        if yield(self[self.keys[i]]) then result[self.keys[i]] = self[self.keys[i]] end
+        if yield(self.keys[i], self[self.keys[i]]) then result[self.keys[i]] = self[self.keys[i]] end
       end
-    else
+    elsif block_given?
       result = Array.new
       for i in 0..self.count - 1
         if yield(self[i]) then result.push(self[i]) end
@@ -38,15 +46,15 @@ module Enumerable
     return result
   end
 
-  def my_all
+  def my_all?
     result = true
-    if self.respond_to?(:keys)
+    if self.respond_to?(:keys) && block_given?
       result = Hash.new
       for i in 0..self.keys.length - 1
-        result = false if !yield(i)
-        break if !yield(i)
+        result = false if !yield(self.keys[i].to_sym, self[self.keys[i]])
+        break if !yield(self.keys[i].to_sym, self[self.keys[i]])
       end
-    else
+    elsif block_given?
       result = Array.new
       for i in 0..self.count - 1
         result = false if !yield(i)
@@ -58,13 +66,13 @@ module Enumerable
 
   def my_any
     result = true
-    if self.respond_to?(:keys)
+    if self.respond_to?(:keys) && block_given?
       result = Hash.new
       for i in 0..self.keys.length - 1
         result = false if !yield(i)
         break if !yield(i)
       end
-    else
+    elsif block_given?
       result = Array.new
       for i in 0..self.count - 1
         result = false if !yield(i)
@@ -74,19 +82,17 @@ module Enumerable
     return result
   end
 
-  def my_none
-    result = true
-    if self.respond_to?(:keys)
-      result = Hash.new
+  def my_none?
+    result = false
+    if self.respond_to?(:keys) && block_given?
       for i in 0..self.keys.length - 1
         result = false if !yield(i)
         break if !yield(i)
       end
-    else
-      result = Array.new
+    elsif block_given?
       for i in 0..self.count - 1
-        result = false if !yield(i)
-        break if !yield(i)
+        result = true if yield(self[i])
+        break if yield(self[i])
       end
     end
     return !result
@@ -94,32 +100,31 @@ module Enumerable
 
   def my_count
     if self.respond_to?(:keys)
-      return self.keys.length
+      self.keys.length
     else
-      return self.count
+      self.count
     end
-    return result
   end
 
   def my_map
-    if self.respond_to?(:keys)
+    if self.respond_to?(:keys) && block_given?
+      result = Array.new
       for i in 0..self.keys.length - 1
-        self[self.keys[i]] = yield(self[self.keys[i]])
+        result[i] = yield(self.keys[i], self[self.keys[i]])
       end
-    else
+      result
+    elsif block_given?
       for i in 0..self.count - 1
         self[i] = yield(self[i])
       end
+      self
     end
   end
 
   def my_inject
-    if self.respond_to?(:keys)
-      result = self[self.keys[0]]
-      for i in 1..self.keys.length - 1
-        result = yield(result, self[self.keys[i]])
-      end
-    else
+    if self.respond_to?(:keys) && block_given?
+      raise "expected Array, got Hash"
+    elsif block_given?
       result = self[0]
       for i in 1..self.count - 1
         result = yield(result, self[i])
@@ -127,6 +132,10 @@ module Enumerable
     end
     return result
   end
+end
+
+def multiply_els(arr)
+  arr.inject { |sum, n| sum * n }
 end
 
 class Array
@@ -137,7 +146,74 @@ class Hash
   include Enumerable
 end
 
-[1, 2, 3, 4, 5].my_each do |value| p value end
-asd = [1, 2, 3, 4, 5].my_select do |value| value > 2 end
-p asd
-p (5..10).inject { |sum, n| sum + n }
+testhash = {
+  "a" => 1,
+  "b" => 2,
+  "c" => 3,
+
+}
+print "\nBEGIN HASH TESTING\n\n"
+testhash.each { |key, value| p "key: #{key} => value #{value}" }
+testhash.my_each { |key, value| p "key: #{key} => value #{value}" }
+print "\n"
+testhash.each_with_index { |key, value| p "key: #{key} => value #{value}" }
+testhash.my_each_with_index { |key, value| p "key: #{key} => value #{value}" }
+print "\n"
+p testhash.select { |key, value| value > 1 }
+p testhash.my_select { |key, value| value > 1 }
+print "\n"
+p testhash.all? { |key, value| value > 1 }
+p testhash.my_all? { |key, value| value > 1 }
+print "\n"
+p testhash.count
+p testhash.my_count
+
+print "\n"
+p testhash.map { |key, value| value * value }
+p testhash.my_map { |key, value| value * value }
+
+print "\n"
+p [2, 4, 5].inject { |sum, value| sum * value }
+p multiply_els([2, 4, 5])
+
+print "\nEND HASH TESTING\n\n\n"
+
+testarray = [1, 2, 3, 4]
+print "\nBEGIN ARRAY TESTING\n"
+print "\n"
+testarray.each { |value| p "value #{value}" }
+testarray.my_each { |value| p "value #{value}" }
+
+print "\n"
+testarray.each_with_index { |value| p "value #{value}" }
+testarray.my_each_with_index { |value| p "value #{value}" }
+
+print "\n"
+p testarray.select { |value| value > 1 }
+p testarray.my_select { |value| value > 1 }
+
+print "\n"
+p testarray.all? { |value| value > 1 }
+p testarray.my_all? { |value| value > 1 }
+
+print "\n"
+p testarray.none? { |value| value > 1 }
+p testarray.my_none? { |value| value > 1 }
+
+print "\n"
+p testarray.count
+p testarray.my_count
+
+print "\n"
+p testarray.map { |n| n * n }
+p testarray.my_map { |n| n * n }
+
+print "\n"
+p testarray.inject { |sum, n| sum + n }
+p testarray.my_inject { |sum, n| sum + n }
+
+print "\n"
+p [2, 4, 5].inject { |sum, n| sum * n }
+p multiply_els([2, 4, 5])
+
+print "\nEND ARRAY TESTING\n"
